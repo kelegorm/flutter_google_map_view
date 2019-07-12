fb876ff6750ffb5ae96531886d4d64c40a9ba9e1
Dmitry <kelegorm@users.noreply.github.com>
2019-07-12 19:44:32 +0300

    Updated google maps header to new

--- ios/Classes/MapViewController.h
+++ ios/Classes/MapViewController.h
@@ -7,8 +7,7 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
-#import <GoogleMaps/GoogleMaps/GMSMapView.h>
-#import <GoogleMaps/GoogleMaps/GMSIndoorDisplay.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @class MapViewPlugin;
 @class MapAnnotation;
